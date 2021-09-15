9e8322d756020713e850a6328af9d3c849864f2b
qspp960 <qspp@daum.net>
2021-09-15 12:16:03 +0900

    product-init

---
+++ src/main/index.js
@@ -0,0 +1,53 @@
+import './index.css'
+import axios from "axios";
+import React from 'react';
+
+function MainPage() {
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(
+        function () {
+
+        }, []);
+    return (
+        <div>
+            <div id="header">
+                <div id="header-area">
+                    <img src="images/icons/logo.png" />
+                </div>
+            </div>
+            <div id="body">
+                <div id="banner">
+                    <img src="images/banners/banner1.png" />
+                </div>
+                <h1>판매 상품들</h1>
+                <div id="product-list">
+                    {
+                        products.map(function (product, index) {
+                            return (
+                                <div className="product-card">
+                                    <div>
+                                        <img className="product-img" src={product.imageUrl} />
+                                    </div>
+                                    <div className="product-contents">
+                                        <span className="product-name">{product.name}</span>
+                                        <span className="product-price">{product.price}</span>
+                                        <div className="product-seller">
+                                            <img
+                                                className="product-avatar"
+                                                src="images/icons/avatar.png"
+                                            />
+                                        </div>
+                                    </div>
+                                    )
+                        });
+                    }
+                                </div>
+            </div>
+                <div id="footer"></div>
+
+            </div>
+            );
+
+}
+
+            export default MainPage;
